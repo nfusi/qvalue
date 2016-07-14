2e99d1e138d67934a9848a1115cfa5c7abbe40e7
mqbssaby <alexis.boukouvalas@gmail.com>
2016-07-14 10:32:03 +0100

    pep 8

--- qvalue/qvalue.py
+++ qvalue/qvalue.py
@@ -1,10 +1,7 @@
-import numpy as np
 import scipy as sp
-import sys, pickle, pdb
-import scipy.stats as st
-import scipy.interpolate
 
-def estimate(pv, m = None, verbose = False, lowmem = False, pi0 = None):
+
+def estimate(pv, m=None, verbose=False, lowmem=False, pi0=None):
     """
     Estimates q-values from p-values
 
@@ -14,52 +11,47 @@ def estimate(pv, m = None, verbose = False, lowmem = False, pi0 = None):
     m: number of tests. If not specified m = pv.size
     verbose: print verbose messages? (default False)
     lowmem: use memory-efficient in-place algorithm
-    pi0: if None, it's estimated as suggested in Storey and Tibshirani, 2003. 
+    pi0: if None, it's estimated as suggested in Storey and Tibshirani, 2003.
          For most GWAS this is not necessary, since pi0 is extremely likely to be
          1
 
     """
-    
-    
     assert(pv.min() >= 0 and pv.max() <= 1), "p-values should be between 0 and 1"
 
     original_shape = pv.shape
-    pv = pv.ravel() # flattens the array in place, more efficient than flatten() 
+    pv = pv.ravel()  # flattens the array in place, more efficient than flatten()
 
-    if m == None:
+    if m is None:
         m = float(len(pv))
     else:
         # the user has supplied an m
         m *= 1.0
 
     # if the number of hypotheses is small, just set pi0 to 1
-    if len(pv) < 100 and pi0 == None:
+    if len(pv) < 100 and pi0 is None:
         pi0 = 1.0
-    elif pi0 != None:
+    elif pi0 is not None:
         pi0 = pi0
     else:
         # evaluate pi0 for different lambdas
         pi0 = []
         lam = sp.arange(0, 0.90, 0.01)
         counts = sp.array([(pv > i).sum() for i in sp.arange(0, 0.9, 0.01)])
-        
         for l in range(len(lam)):
             pi0.append(counts[l]/(m*(1-lam[l])))
 
         pi0 = sp.array(pi0)
 
         # fit natural cubic spline
-        tck = sp.interpolate.splrep(lam, pi0, k = 3)
+        tck = sp.interpolate.splrep(lam, pi0, k=3)
         pi0 = sp.interpolate.splev(lam[-1], tck)
-        
+
         if pi0 > 1:
             if verbose:
                 print("got pi0 > 1 (%.3f) while estimating qvalues, setting it to 1" % pi0)
-            
             pi0 = 1.0
 
     assert(pi0 >= 0 and pi0 <= 1), "pi0 is not between 0 and 1: %f" % pi0
-
 
     if lowmem:
         # low memory version, only uses 1 pv and 1 qv matrices
@@ -77,14 +69,14 @@ def estimate(pv, m = None, verbose = False, lowmem = False, pi0 = None):
             prev_qv = qv[cur_max]
 
     else:
-        p_ordered = sp.argsort(pv)    
+        p_ordered = sp.argsort(pv)
         pv = pv[p_ordered]
         qv = pi0 * m/len(pv) * pv
-        qv[-1] = min(qv[-1],1.0)
+        qv[-1] = min(qv[-1], 1.0)
 
         for i in xrange(len(pv)-2, -1, -1):
             qv[i] = min(pi0*m*pv[i]/(i+1.0), qv[i+1])
-        
+
         # reorder qvalues
         qv_temp = qv.copy()
         qv = sp.zeros_like(qv)
@@ -92,5 +84,5 @@ def estimate(pv, m = None, verbose = False, lowmem = False, pi0 = None):
 
     # reshape qvalues
     qv = qv.reshape(original_shape)
-        
+
     return qv
